0b77deb8597f8e60a447e568a6a0c1187fabac57
TheForeverOptimist <sean@elevate.app>
2023-03-10 06:08:54 -0500

    Set up express,path,methodoverride

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const express = require('express');
+const path = require('path')
+const { nextTick } = require('process');
+const methodOverride = require('method-override');
+
 const app = express();
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
