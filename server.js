5fa0a08e68e6af00de381f185c3accebe66f10cf
TheForeverOptimist <sean@elevate.app>
2023-03-10 07:42:23 -0500

    views and controllers setup

--- server.js
+++ server.js
@@ -8,15 +8,20 @@ const influencerController = require('./controllers/influencers')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use('/influencers', influencerController);
-
 app.get('/', (req,res) => {
     res.render('home.ejs');
 })
 
+
+app.use('/influencers', influencerController);
+// app.get('/influencers', (req,res) => {
+//     res.render('influencers.ejs');
+// })
 app.get('/*', (req,res) => {
     res.render('404.ejs');
 })
+
+
 
 app.listen(4000, () => {
     console.log('Listening on Port 4000');
